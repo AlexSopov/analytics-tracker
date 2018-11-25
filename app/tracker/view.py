e77330029113288a4cc1f841c612e9ca8c73a3c9
Alex Sopov <leshasopov1@gmail.com>
2018-11-25 20:47:59 +0200

    Catch project name on /track request

--- app/tracker/view.py
+++ app/tracker/view.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 track_blueprint = Blueprint('track', __name__)
 
 
-@track_blueprint.route("/track", methods=['GET'])
-def get_all():
-    return ""
+@track_blueprint.route("/track/<string:project>", methods=['GET'])
+def get_all(project):
+    return project
