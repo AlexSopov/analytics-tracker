274aab75cabdb00e06a7370fcdd6bb3c9ff4d97d
Alex Sopov <leshasopov1@gmail.com>
2018-11-25 21:01:08 +0200

    Add configurations to project

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask
+from config import Config
 
 app = Flask('analytics-tracker')
 app.secret_key = os.urandom(24)
-
+app.config.from_object(Config)
 
 from app.tracker.view import track_blueprint
 app.register_blueprint(track_blueprint, url_prefix='/')
